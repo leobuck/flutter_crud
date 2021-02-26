11c8967b78fba21206581e09abee681fde743cf9
Leonardo Buck de Godoy <leonardo@timais.com>
2021-02-26 11:30:55 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@email.com',
+    avatarUrl: 'https://static.thenounproject.com/png/363640-200.png'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@email.com',
+    avatarUrl: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@email.com',
+    avatarUrl: 'https://static.thenounproject.com/png/363640-200.png'
+  )
+};
